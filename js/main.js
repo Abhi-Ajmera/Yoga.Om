7b2178f97a1633e9ce90a30cf57b7f0e42e841f9
Abhishek Ajmera <113019974+Abhi-Ajmera@users.noreply.github.com>
2023-01-07 12:33:59 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    let nav = document.querySelector("#nav");
+        if (nav.style.display === "none") {
+            nav.style.display = "block";
+          } else {
+            nav.style.display = "none";
+          }
+}
